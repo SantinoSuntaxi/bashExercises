043d4edc1fc4d8f20ddfb0caebdf013034716b48
ghmafla <40498642+ghmafla@users.noreply.github.com>
2022-01-08 19:48:19 -0500

    update script3

--- script3.sh
+++ script3.sh
@@ -5,41 +5,23 @@
 #El primer y segundo argumentos, ambos en la misma línea.
 #Si hay más de dos argumentos, los argumentos a partir del tercero (este incluido) cada uno en una línea.
 clear
-echo "*********************************************************************"
-echo "*                _____   ___ _    _    ___   ___   ___              *"
-echo "*               |     | |___  \  /  _ |   | |___| |___              *"
-echo "*               |_____| |___   \/     |___| |      ___|             *"
-echo "*                                                                   *"
-echo "***UNIR Ecuador | Universidad Internacional de La Rioja en Ecuador***"
-echo "*                                                                   *"
-echo "***************Administración de Sistemas para la Cloud**************"
-echo "*                                                                   *"
-echo "* Actividad grupal: Programación en Bash                            *"
-echo "*                                                                   *"
-echo "* Integrantes:                                                      *"
-echo "*             > Pedro Santino Suntaxi Cocanguilla                   *"
-echo "*             > Washingtong David Pena Sanchez                      *"
-echo "*             > David Chimbo Vega                                   *"
-echo "*             > Gabriel Hernan Mafla Cabascango                     *"
-echo "*                                                                   *"
-echo "*********************************************************************"
+echo " Integrantes:   > Pedro Santino Suntaxi Cocanguilla"
+echo "                > Washingtong David Pena Sanchez"
+echo "                > David Chimbo Vega                     "
+echo "                > Gabriel Hernan Mafla Cabascango"
 echo
-
 #Imprime el script que esta ejecutandose
 script_name1=$(basename $0)
 echo "Script ejecutandose --> $script_name1 "
 echo
 parm12=""
-
 #Imprime el numero de argumentos que se han pasado
-
 if [ $# -eq 0 ]; then
     echo "No se ingresaron parametros"
 else
     echo "--> Mostrando los parametros ingresados <--"
     i=1
     for parametro in "$@"; do
-
         if (($i == 1 && $# == 1)); then
             echo "Parametro $i: $parametro"
         else
@@ -50,20 +32,16 @@ else
         if (($i == 2 && $# == 2)); then
             parm12="$parm12 Parametro $i $parametro"
             echo $parm12
-        else
-            if (($i == 2 && $# > 2)); then
-                parm12="$parm12 Parametro $i $parametro"
-            else
-                if (($i == 3 && $# > 2)); then
-                    echo $parm12
-                    echo "Parametro $i: $parametro"
-                else
-                    if (($i > 3)); then
-                        echo "Parametro $i: $parametro"
-                    fi
-                fi
-            fi
-
+        else if (($i == 2 && $# > 2)); then
+					parm12="$parm12 Parametro $i $parametro"
+				else if (($i == 3 && $# > 2)); then
+							echo $parm12
+							echo "Parametro $i: $parametro"
+						else  if (($i > 3)); then
+									echo "Parametro $i: $parametro"
+								fi
+						fi
+				fi
         fi
         i=$((i + 1))
     done
