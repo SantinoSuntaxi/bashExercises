6a60965fb9aa864da0410bb5389582bb9c38769e
ghmafla <40498642+ghmafla@users.noreply.github.com>
2022-01-08 19:55:30 -0500

    update script5

--- script5.sh
+++ script5.sh
@@ -1,34 +1,16 @@
-
 #!/bin/bash
 
-#Escribid un script en Bash que acepte una ruta a un fichero o un directorio e imprima por pantalla si es un fichero normal, 
-#un directorio u otro tipo de fichero. Finalmente, el script ejecutará el comando ls sobre esta ruta en formato largo
+# Script que renombra todos los ficheros con extensión JPG del directorio actual, añadiendo 
+# un prefijo con la fecha en formato año, mes, día. Por ejemplo, un fichero con nombre imagen1.jpg 
+# pasaría a llamarse 20200413-image1.jpg, si el script se ejecuta el 13 de abril de 2020.
 clear
 echo -e '\e[1;32m';
-echo "*********************************************************************";
-echo "*                _____   ___ _    _    ___   ___   ___              *";
-echo "*               |     | |___  \  /  _ |   | |___| |___              *";
-echo "*               |_____| |___   \/     |___| |      ___|             *";
-echo "*                                                                   *";
-echo "***UNIR Ecuador | Universidad Internacional de La Rioja en Ecuador***";
-echo "*                                                                   *";
-echo "***************Administración de Sistemas para la Cloud**************";
-echo "*                                                                   *";
-echo "* Actividad grupal: Programación en Bash                            *";
-echo "*                                                                   *";
-echo "* Integrantes:                                                      *";
-echo "*             > Pedro Santino Suntaxi Cocanguilla                   *";
-echo "*             > Washingtong David Pena Sanchez                      *";
-echo "*             > David Chimbo Vega                                   *";
-echo "*             > Gabriel Hernan Mafla Cabascango                     *";
-echo "*                                                                   *";
-echo "*********************************************************************";
-echo
+echo " Integrantes:   > Pedro Santino Suntaxi Cocanguilla"
+echo "                > Washingtong David Pena Sanchez"
+echo "                > David Chimbo Vega                     "
+echo "                > Gabriel Hernan Mafla Cabascango"
 echo -e '\e[m';
 echo
-echo "Script que renombra todos los ficheros con extensión JPG del directorio actual, añadiendo 
-un prefijo con la fecha en formato año, mes, día. Por ejemplo, un fichero con nombre imagen1.jpg 
-pasaría a llamarse 20200413-image1.jpg, si el script se ejecuta el 13 de abril de 2020."
 echo
 echo -e '\e[1;32m';
 echo "Continue o pulse Cntrl+C para omitir"
@@ -46,11 +28,8 @@ done
 echo -e '\e[m';
 echo
 echo "FICHEROS RENOMBRADOS: " 
-
-
  
 for fichero in `ls *.jpg`
 do
 echo " > " $fichero
 done
-
